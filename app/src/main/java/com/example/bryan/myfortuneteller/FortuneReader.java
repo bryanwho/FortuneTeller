8b2282d64121681676cefc271b862ebb50e82145
Bryan <bryan@beginnerprogrammers.nyc>
2015-11-18 10:55:00 -0500

    added repo

---
+++ app/src/main/java/com/example/bryan/myfortuneteller/FortuneReader.java
@@ -0,0 +1,109 @@
+package com.example.bryan.myfortuneteller;
+
+import android.content.Context;
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+
+import java.util.Locale;
+import java.util.Random;
+
+/**
+ * Created by bryan on 10/11/15.
+ */
+public class FortuneReader implements TextToSpeech.OnInitListener{
+
+    private TextToSpeech tts;
+    private Context context;
+    private String response = "";
+
+    public FortuneReader(Context context) {
+        this.context = context;
+    }
+
+    public void readFortune(String question) {
+
+        tts = new TextToSpeech(context, this);
+        tts.setLanguage(Locale.US);
+        tts.setPitch((float) 2);
+
+        String questionEdit = question.toLowerCase();
+        if(questionEdit.contains("who") ||
+                questionEdit.contains("what") ||
+                questionEdit.contains("where")||
+                questionEdit.contains("why") ||
+                questionEdit.contains("how") ||
+                questionEdit.contains("when") ||
+                questionEdit.contains("will")||
+                questionEdit.contains("was")) {
+
+            response = getNiceAnswer();
+        } else {
+            response = getNastyAnswer();
+        }
+    }
+
+    public String getNiceAnswer() {
+        String answer = "outlook unclear";
+        Random randomNumber = new Random();
+
+        int number = randomNumber.nextInt(4) + 1;
+
+        switch(number) {
+            case 1:
+                answer = "Most definitely";
+                break;
+            case 2:
+                answer = "Good chance";
+                break;
+            case 3:
+                answer = "Anything is possible if you drink enough";
+                break;
+            case 4:
+                answer = "Of course";
+                break;
+        }
+
+        return answer;
+    }
+
+    public String getNastyAnswer() {
+        String answer = "outlook unclear";
+        Random randomNumber = new Random();
+
+        int number = randomNumber.nextInt(4) + 1;
+
+        switch(number) {
+            case 1:
+                answer = "Stupidity is a virtue, dumbass";
+                break;
+            case 2:
+                answer = "Fuck you, Fuck who you are, fuck who you were";
+                break;
+            case 3:
+                answer = "I'm going to pop a cap in your ass";
+                break;
+            case 4:
+                answer = "Why don't you ask me a real question, dick-head";
+                break;
+        }
+
+        return answer;
+    }
+
+
+
+    @Override
+    public void onInit(int status) {
+        if(status == TextToSpeech.SUCCESS){
+            Log.d("flow", "success");
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                tts.speak(response, TextToSpeech.QUEUE_FLUSH, null, null);
+            } else {
+                tts.speak(response, TextToSpeech.QUEUE_ADD, null);
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bryan/myfortuneteller/MainActivityFragment.java
@@ -0,0 +1,92 @@
+package com.example.bryan.myfortuneteller;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment implements View.OnClickListener {
+
+    TextView textView;
+    FortuneReader fortuneReader;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.fragment_main, container, false);
+
+        textView = (TextView) layout.findViewById(R.id.textView);
+        textView.setOnClickListener(this);
+
+        return layout;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.textView:
+                Log.d("flow", "textview clicked");
+                displaySpeechRecognizer();
+                break;
+        }
+    }
+
+    private static final int SPEECH_REQUEST_CODE = 0;
+
+    // Create an intent that can start the Speech Recognizer activity
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+    // Start the activity, the intent will be populated with the speech text
+        Log.d("flow", "start intent");
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == SPEECH_REQUEST_CODE) {
+
+            if(resultCode == Activity.RESULT_OK) {
+
+                List<String> results = data.getStringArrayListExtra(
+                        RecognizerIntent.EXTRA_RESULTS);
+                String spokenText = results.get(0);
+
+                fortuneReader = new FortuneReader(getActivity());
+                fortuneReader.readFortune(spokenText);
+            } else {
+                Log.d("flow", "failure");
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
